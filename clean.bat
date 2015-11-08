200d7a714387dedaa53f0272ceb33f5819e9d2d6
Thomas Fussell <thomas.fussell@gmail.com>
2015-11-08 11:26:22 -0500

    clean up configure scripts

--- clean.bat
+++ clean.bat
@@ -4,4 +4,4 @@ for /f %%i in ('where python') DO (set PYTHON=%%i) & goto :done1
 :done1
 @where python3 > nul 2>&1 && for /f %%i in ('@where python3') DO (@set PYTHON=%%i) & goto :done2
 :done2
-!PYTHON! clean
+!PYTHON! configure %*
