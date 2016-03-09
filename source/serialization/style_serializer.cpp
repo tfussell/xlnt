ce7b30d7ab530c517c2c81054c1b7e86c9483b5f
Thomas Fussell <thomas.fussell@gmail.com>
2016-03-09 14:02:14 +0800

    wrong header...

--- source/serialization/style_serializer.cpp
+++ source/serialization/style_serializer.cpp
@@ -21,7 +21,7 @@
 //
 // @license: http://www.opensource.org/licenses/mit-license.php
 // @author: see AUTHORS file
-#include <locale>
+#include <cctype>
 
 #include <xlnt/serialization/style_serializer.hpp>
 #include <xlnt/serialization/xml_document.hpp>
