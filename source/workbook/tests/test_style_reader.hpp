959d246995e25045c24628a61dd31f6cd9d2c943
Thomas Fussell <thomas.fussell@gmail.com>
2016-07-13 23:12:49 -0400

    start testing writing styles

---
+++ source/workbook/tests/test_style_reader.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <cxxtest/TestSuite.h>
+
+#include <detail/style_serializer.hpp>
+#include <detail/stylesheet.hpp>
+#include <detail/workbook_impl.hpp>
+#include <helpers/path_helper.hpp>
+
+class test_style_reader : public CxxTest::TestSuite
+{
+public:
+    void test_complex_formatting()
+    {
+        xlnt::workbook wb;
+        wb.load(PathHelper::GetDataDirectory("/reader/formatting.xlsx"));
+        
+        // border_style
+        TS_ASSERT_EQUALS(wb.get_active_sheet().get_cell("E30").get_border().get_top()->get_color(), xlnt::color(xlnt::color::type::indexed, 10));
+        TS_ASSERT_EQUALS(wb.get_active_sheet().get_cell("E30").get_border().get_top()->get_border_style(), xlnt::border_style::thin);
+        
+        // underline_style
+        TS_ASSERT_EQUALS(wb.get_active_sheet().get_cell("E30").get_font().get_underline(), xlnt::font::underline_style::none);
+        TS_ASSERT_EQUALS(wb.get_active_sheet().get_cell("F30").get_font().get_underline(), xlnt::font::underline_style::single);
+    }
+};
