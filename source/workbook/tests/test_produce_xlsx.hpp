ec42f3c3aca70e27e6b2c3ae3fd930e59789b1ef
Thomas Fussell <thomas.fussell@gmail.com>
2016-11-27 18:41:46 +0100

    debug gcc exception

--- source/workbook/tests/test_produce_xlsx.hpp
+++ source/workbook/tests/test_produce_xlsx.hpp
@@ -162,12 +162,13 @@ public:
 		formatted_run.set_string("Sheet1 comment");
 		comment_text.add_run(formatted_run);
 		sheet1.get_cell("A1").comment(xlnt::comment(comment_text, "Microsoft Office User"));
-
+/*
 		sheet1.get_cell("A2").set_value("Sheet1!A2");
 		formatted_run.set_string("Sheet1 comment2");
 		comment_text.clear();
 		comment_text.add_run(formatted_run);
 		sheet1.get_cell("A2").comment(xlnt::comment(comment_text, "Microsoft Office User"));
+*/
 /*
 		auto sheet2 = wb.create_sheet();
 		sheet2.get_cell("A1").set_value("Sheet2!A1");
