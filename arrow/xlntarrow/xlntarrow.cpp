fa179d58abd9f1a7251105450a0cd816079ed64a
Thomas Fussell <thomas.fussell@gmail.com>
2017-07-05 15:06:31 -0700

    add placeholder implementation of arrow2xlsx

--- arrow/xlntarrow/xlntarrow.cpp
+++ arrow/xlntarrow/xlntarrow.cpp
@@ -35,24 +35,8 @@ void arrow2xlsx(const ::arrow::Table &table, std::ostream &s)
   xlnt::streaming_workbook_writer writer;
   writer.open(s);
 
-  writer.begin_worksheet();
-
-  while (reader.has_cell())
-  {
-      auto cell = reader.read_cell();
-
-      if (first_row < 1)
-      {
-          first_row = cell.row();
-      }
-
-      if (cell.reference().row() % 1000 == 1)
-      {
-          std::cout << cell.reference().to_string() << std::endl;
-      }
-  }
-
-  reader.end_worksheet();
+  writer.add_worksheet("Sheet1");
+  writer.add_cell("A1").value("test");
 }
 
 }
