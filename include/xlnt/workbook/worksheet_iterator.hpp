744dd0afbbe064dc7dc3d92c77b9306560cbe742
Joshua <joshua.crawford@levno.com>
2018-05-30 07:52:54 +1200

    Remove uses of std::iterator (deprecated in C++17)
    Detailed reasoning for the deprecation is provided by the paper proposing deprecation (http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0174r2.html) and a related LWG issue (https://cplusplus.github.io/LWG/issue2438).
    
    This was the only issue preventing a clean compile with VS 15.7.2 with c++17/c++latest set as the target language
    
    The issue could be resolved in two ways. Providing a custom replacement to std::iterator (a very simple structure) or by providing the 5 required typedefs. The only functional difference from my reading is that the typedefs are not immediately available to the implementer with the inheritance. I find the inline typedefs to be clearer hence the selection in this commit

--- include/xlnt/workbook/worksheet_iterator.hpp
+++ include/xlnt/workbook/worksheet_iterator.hpp
@@ -37,18 +37,22 @@ class worksheet;
 // because one needs to point at a const workbook and the other needs
 // to point at a non-const workbook stored as a member variable, respectively.
 
-/// <summary>
-/// Alias the parent class of this iterator to increase clarity.
-/// </summary>
-using ws_iter_type = std::iterator<std::bidirectional_iterator_tag,
-    worksheet, std::ptrdiff_t, worksheet *, worksheet>;
 
 /// <summary>
 /// An iterator which is used to iterate over the worksheets in a workbook.
 /// </summary>
-class XLNT_API worksheet_iterator : public ws_iter_type
+class XLNT_API worksheet_iterator
 {
 public:
+    /// <summary>
+    /// iterator tags required for use with standard algorithms and adapters
+    /// </summary>
+    using iterator_category = std::bidirectional_iterator_tag;
+    using value_type = worksheet;
+    using difference_type = std::ptrdiff_t;
+    using pointer = worksheet *;
+    using reference = worksheet; // intentionally value
+
     /// <summary>
     /// Constructs a worksheet iterator from a workbook and sheet index.
     /// </summary>
@@ -107,17 +111,20 @@ private:
 
 
 /// <summary>
-/// Alias the parent class of this iterator to increase clarity.
-/// </summary>
-using c_ws_iter_type = std::iterator<std::bidirectional_iterator_tag,
-    const worksheet, std::ptrdiff_t, const worksheet *, const worksheet>;
-
-/// <summary>
 /// An iterator which is used to iterate over the worksheets in a const workbook.
 /// </summary>
-class XLNT_API const_worksheet_iterator : public c_ws_iter_type
+class XLNT_API const_worksheet_iterator
 {
 public:
+    /// <summary>
+    /// iterator tags required for use with standard algorithms and adapters
+    /// </summary>
+    using iterator_category = std::bidirectional_iterator_tag;
+    using value_type = const worksheet;
+    using difference_type = std::ptrdiff_t;
+    using pointer = const worksheet *;
+    using reference = const worksheet; // intentionally value
+
     /// <summary>
     /// Constructs a worksheet iterator from a workbook and sheet index.
     /// </summary>
