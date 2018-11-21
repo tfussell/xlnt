38f12ff8465924989e513c823a3b11ebec802140
Kostas Dizas <254960+kostasdizas@users.noreply.github.com>
2018-11-21 12:58:48 +0000

    Serialise phonetic runs and properties

--- source/detail/serialization/xlsx_consumer.cpp
+++ source/detail/serialization/xlsx_consumer.cpp
@@ -2999,11 +2999,34 @@ rich_text xlsx_consumer::read_rich_text(const xml::qname &parent)
         }
         else if (text_element == xml::qname(xmlns, "rPh"))
         {
-            skip_remaining_content(text_element);
+            phonetic_run pr;
+            pr.start = parser().attribute<std::uint32_t>("sb");
+            pr.end = parser().attribute<std::uint32_t>("eb");
+
+            expect_start_element(xml::qname(xmlns, "t"), xml::content::simple);
+            pr.text = read_text();
+
+            if (parser().attribute_present(xml_space))
+            {
+                pr.preserve_space = parser().attribute(xml_space) == "preserve";
+            }
+
+            expect_end_element(xml::qname(xmlns, "t"));
+
+            t.add_phonetic_run(pr);
         }
         else if (text_element == xml::qname(xmlns, "phoneticPr"))
         {
-            skip_remaining_content(text_element);
+            phonetic_pr ph(parser().attribute<std::uint32_t>("fontId"));
+            if (parser().attribute_present("type"))
+            {
+                ph.type(phonetic_pr::type_from_string(parser().attribute("type")));
+            }
+            if (parser().attribute_present("alignment"))
+            {
+                ph.alignment(phonetic_pr::alignment_from_string(parser().attribute("alignment")));
+            }
+            t.phonetic_properties(ph);
         }
         else
         {
