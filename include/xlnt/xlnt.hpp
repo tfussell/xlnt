bc9d3a2371513008dfa94c8f93363265a00522ee
Дмитрий <dmitrii-1997@outlook.com>
2019-04-26 13:29:30 +0300

    Add changes for VS.

--- include/xlnt/xlnt.hpp
+++ include/xlnt/xlnt.hpp
@@ -90,3 +90,28 @@
 #include <xlnt/worksheet/sheet_protection.hpp>
 #include <xlnt/worksheet/sheet_view.hpp>
 #include <xlnt/worksheet/worksheet.hpp>
+
+#ifdef _MSC_VER
+
+#if defined(_IA64_) || defined(_AMD64_)
+#define __XLNT_PLATFORM "x64"
+#else
+#define __XLNT_PLATFORM "x86"
+#endif
+
+#ifdef _DEBUG
+#define __XLNT_DEBUG "Debug"
+#define __XLNT_DEBUG_SHORT "d"
+#else
+#define __XLNT_DEBUG "Release"
+#define __XLNT_DEBUG_SHORT ""
+#endif
+
+#define __XLNT_LIB_NAME __XLNT_PLATFORM "-" __XLNT_DEBUG "\\source\\xlnt" __XLNT_DEBUG_SHORT ".lib"
+
+#if defined(_GTL_AUTO_LINK)
+#pragma comment(lib, __XLNT_LIB_NAME)
+#pragma message("Automatically linking with " __XLNT_LIB_NAME)
+#endif
+
+#endif
